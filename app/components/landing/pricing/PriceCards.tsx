b1a49408162670fd65efcfedac42823d231d36d0
enricomilli <enricomilli666@gmail.com>
2025-02-25 09:22:29 +0000

    Revert "decision: shutdown for now"
    This reverts commit cb42dc5f462c3a29ebbefbb2b31303be5baa73f0.

--- app/components/landing/pricing/PriceCards.tsx
+++ app/components/landing/pricing/PriceCards.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import { Link } from '@remix-run/react'
 import { Check } from 'lucide-react'
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { weeklyPricing, monthlyPricing } from '@/constants/constants'
 
@@ -32,40 +26,28 @@ const PriceCard: React.FC<PriceCardProps> = ({ item }) => {
         <Card className='h-full max-w-[39rem] p-3 max-lg:w-full lg:w-auto'>
             <CardHeader>
                 <CardTitle className='text-primary-content'>{item.title}</CardTitle>
-                <CardDescription className='text-primary-content/90 max-w-[400px]'>
-                    {item.description}
-                </CardDescription>
+                <CardDescription className='text-primary-content/90 max-w-[400px]'>{item.description}</CardDescription>
             </CardHeader>
             <CardContent>
                 <div className='mb-3 flex h-[5.5rem] items-center'>
                     {item.price !== null && (
                         <>
                             <span className='text-3xl'>$</span>
-                            <span className='text-[4.0rem] font-bold leading-none md:text-[4.5rem]'>
-                                {item.price}
-                            </span>
-                            {item.hasPriceDesc && (
-                                <span className='ml-1 text-xl md:text-2xl'>
-                                    {item.priceDescription}
-                                </span>
-                            )}
+                            <span className='text-[4.0rem] font-bold leading-none md:text-[4.5rem]'>{item.price}</span>
+                            {item.hasPriceDesc && <span className='ml-1 text-xl md:text-2xl'>{item.priceDescription}</span>}
                         </>
                     )}
                 </div>
 
-                {/* <Link to={item.url}> */}
-                <Button disabled size={'lg'} className={`mb-7 w-full`}>
-                    {/* {item.buttonText} */}
-                    Coming Soon
-                </Button>
-                {/* </Link> */}
+                <Link to={item.url}>
+                    <Button size={'lg'} className={`mb-7 w-full`}>
+                        {item.buttonText}
+                    </Button>
+                </Link>
 
                 <ul className='space-y-4'>
                     {item.features.map((feature, index) => (
-                        <li
-                            key={index}
-                            className='border-n-6 flex items-center justify-start border-t pt-4'
-                        >
+                        <li key={index} className='border-n-6 flex items-center justify-start border-t pt-4'>
                             <Check className='mr-4 mt-1' size={24} />
                             <p>{feature}</p>
                         </li>
@@ -88,10 +70,6 @@ const PricingCards: React.FC<PricingCardsProps> = ({ pricingData }) => {
     )
 }
 
-export const WeeklyPriceCard: React.FC = () => (
-    <PricingCards pricingData={weeklyPricing} />
-)
+export const WeeklyPriceCard: React.FC = () => <PricingCards pricingData={weeklyPricing} />
 
-export const MonthlyPriceCard: React.FC = () => (
-    <PricingCards pricingData={monthlyPricing} />
-)
+export const MonthlyPriceCard: React.FC = () => <PricingCards pricingData={monthlyPricing} />
